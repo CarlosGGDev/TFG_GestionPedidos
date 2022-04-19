d2d9e349eca266c87d16f6733eac271f819b50dc
Carlos Gonzalez <carlosgonzalezgomez@hotmail.com>
2022-04-19 23:07:29 +0200

    shopping cart modify items

--- src/main/resources/static/js/newOrder.js
+++ src/main/resources/static/js/newOrder.js
@@ -1,50 +1,78 @@
 $(document).ready(function() {
 
+	// ADD
 	$(document).on('click', '#add', function() {
 
-		if ($(this).closest('tr').children('#product-quantity').children('input').val() > 0) {
-			let name = $(this).closest('tr').children('#product-name').text();
-			let price = $(this).closest('tr').children('#product-price').text();
-			let quantity = $(this).closest('tr').children('#product-quantity').children('input').val();
-			let total = price * quantity;
-			total = total.toFixed(2);
-			let li = `<li class="list-group-item d-flex justify-content-between">
+		if ($(this).closest('tr').children('#td-quantity').children('input').val() > 0) {
+			let name = $(this).closest('tr').children('#td-name').text();
+			let price = $(this).closest('tr').children('#td-price').text();
+			let quantity = $(this).closest('tr').children('#td-quantity').children('input').val();
+			let productTotal = price * quantity;
+			productTotal = productTotal.toFixed(2);
+
+			if (document.getElementById(`${name}`)) {
+				// INCREMENTS ITEM QUANTITY
+				let itemQuantity = $("li[id='" + name + "']").children('#item-quantity').text();
+				let newQuantity = parseInt(itemQuantity) + parseInt(quantity);
+				$("li[id='" + name + "']").children('#item-quantity').text(newQuantity);
+
+				// INCREMENTS ITEM TOTAL
+				productTotal = newQuantity * price;
+				productTotal = productTotal.toFixed(2);
+				$("li[id='" + name + "']").children('#item-total').text(productTotal);
+
+				// Call function to update total
+				// If is not a new item, the price to add to the total is only the cost of new items (the difference)
+				let a = price * quantity
+				updateOrderTotal(a);
+			} else {
+				let li = `<li id="${name}" class="list-group-item d-flex justify-content-between">
 							<div class="col-4">
 								<h6 class="my-0">${name}</h6>
 							</div>
-							<span class="col-3 text-center">${quantity}</span>
-							<span id="product-total" class="col-2">${total} €</span>
+							<span id="item-quantity" class="col-3 text-center">${quantity}</span>
+							<span id="item-total" class="col-3">${productTotal} €</span>
 							<button id="remove" class="btn btn-sm btn-remove col-1"><i class="bi bi-trash3"></i></button>
 						</li>`;
-			$('#cart-footer').before(li);
 
-			// Increment items cart counter
-			let counter = parseFloat($('#products-counter').html()) + 1;
-			$('#products-counter').text(counter);
+				// Add element <li>
+				$('#cart-body').append(li);
 
-			// Add new item price to the total order
-			let orderTotal = parseFloat($('#total').html());
-			orderTotal += parseFloat(total);
-			orderTotal = orderTotal.toFixed(2);
-			$('#total').text(orderTotal);
+				// Call function to update total
+				// If is a new item, the price to add to the total is the new items cost
+				updateOrderTotal(parseFloat(productTotal));
+
+				// Increment items cart counter
+				updateCounter(+1);
+			}
 		}
 	});
 
+	// REMOVE
 	$(document).on('click', '#remove', function() {
+		// Remove item from bag
 		$(this).closest('li').remove();
 
-		let price = $(this).closest('li').children('#product-total').html();
-		console.log(price);
-
-		// Remove new item price to the total order
-		let productTotal = parseFloat($(this).closest('li').children('#product-total').html());
-		let total = $('#total').text() - productTotal;
-		total = total.toFixed(2);
-		$('#total').text(total);
+		// Call function to update total
+		let productTotal = parseFloat($(this).closest('li').children('#item-total').html());
+		updateOrderTotal(-productTotal);
 
 		// Decrease items cart counter
-		let counter = parseFloat($('#products-counter').html()) - 1;
-		$('#products-counter').text(counter);
+		updateCounter(-1);
 	});
 
 });
+
+// UPDATE ITEMS BAG COUNTER
+function updateCounter(num) {
+	let counter = parseFloat($('#items-counter').html()) + num;
+	$('#items-counter').text(counter);
+}
+
+// UPDATE ORDER TOTAL €
+function updateOrderTotal(productTotal) {
+	let total = parseFloat($('#order-total').html());
+	total = total + productTotal;
+	total = total.toFixed(2);
+	$('#order-total').text(total);
+}
